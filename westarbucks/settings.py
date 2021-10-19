17e0f3dbf7153d1b6310cc8dd76adf2bd1d62f9c
Kyuuu827 <lck0827@gmail.com>
2021-10-19 18:23:44 +0900

    WIP

--- westarbucks/settings.py
+++ westarbucks/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'products',
-    'ownersdogs'
+    'ownersdogs',
+    'movies',
 ]
 
 MIDDLEWARE = [
--- westarbucks/urls.py
+++ westarbucks/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('ownersdogs/', include('ownersdogs.urls')),
+    path('movies/', include('movies.urls'))
 ]
